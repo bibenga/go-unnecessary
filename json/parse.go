baf0c9bc422dd066e1491903a6755580e4401218
bibenga <{ID}+{username}@users.noreply.github.com>
2023-09-11 19:37:26 +0000

    adds example for json

---
+++ json/parse.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"log/slog"
+	"os"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile | log.Lmsgprefix)
+	log.SetPrefix("")
+
+	l := slog.Default()
+
+	f, err := os.Open("json/data.json")
+	if err != nil {
+		l.Error("can't open file", "error", err)
+		return
+	}
+	l.Info("file opened")
+	defer f.Close()
+
+	bytes, err := io.ReadAll(f)
+	if err != nil {
+		l.Error("can't read file", "error", err)
+		return
+	}
+	l.Info("json loaded")
+
+	var a = make(map[string]interface{})
+	err = json.Unmarshal(bytes, &a)
+	if err != nil {
+		l.Error("invalid json: %+v", err)
+		return
+	}
+
+	l.Info("json parsed", "data", a)
+}
