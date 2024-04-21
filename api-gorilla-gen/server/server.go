c1ef86da0586873a5ef62f223a09aaae7c9f9a11
bibenga <{ID}+{username}@users.noreply.github.com>
2024-04-21 19:29:30 +0000

    u

--- api-gorilla-gen/server/server.go
+++ api-gorilla-gen/server/server.go
@@ -1,6 +1,7 @@
 // go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest --config=server.cfg.yaml ../api.yaml
 // go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=server.cfg.yaml ../api.yaml
 // go install github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest
+// go install github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@latest
 //
 //go:generate oapi-codegen --config=model.cfg.yaml  ../../api/api.yaml
 //go:generate oapi-codegen --config=client.cfg.yaml ../../api/api.yaml
