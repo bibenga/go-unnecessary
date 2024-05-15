48a284a99131c5318934d59df1fcbe58c34be880
bibenga <{ID}+{username}@users.noreply.github.com>
2024-05-15 20:19:14 +0000

    not worked, an original repo should be cloned

---
+++ llm/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+
+	llama "github.com/go-skynet/go-llama.cpp"
+)
+
+var (
+	threads   = 2
+	tokens    = 128
+	gpulayers = 0
+	seed      = -1
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile | log.Lmsgprefix)
+	log.SetPrefix("")
+
+	l := slog.New(slog.Default().Handler())
+	slog.SetDefault(l)
+
+	model := ""
+
+	l.Info("hello")
+	llm, err := llama.New(
+		model,
+		llama.EnableF16Memory,
+		llama.SetContext(128),
+		llama.EnableEmbeddings,
+		llama.SetGPULayers(gpulayers),
+	)
+	if err != nil {
+		l.Error("Loading the model failed", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+	fmt.Printf("Model loaded successfully.\n")
+
+}
