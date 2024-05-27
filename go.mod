699840a1733534d36a254458b27899fa9fe98c45
bibenga <{ID}+{username}@users.noreply.github.com>
2024-05-27 20:07:06 +0000

    gomponents

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module unnecessary
 
-go 1.22
+go 1.22.0
 
-toolchain go1.22.1
+toolchain go1.22.3
 
 require (
 	firebase.google.com/go/v4 v4.14.0
@@ -35,9 +35,11 @@ require (
 	github.com/gorilla/sessions v1.2.2
 	github.com/gorilla/websocket v1.5.1
 	github.com/gosimple/slug v1.14.0
-	github.com/jackc/pgx/v5 v5.5.5
+	github.com/jackc/pgx/v5 v5.6.0
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/lib/pq v1.10.9
+	github.com/maragudk/gomponents v0.20.2
+	github.com/maragudk/gomponents-htmx v0.5.0
 	github.com/mozillazg/go-unidecode v0.2.0
 	github.com/oapi-codegen/runtime v1.1.1
 	github.com/qor5/web v1.3.2
@@ -51,7 +53,9 @@ require (
 	github.com/stretchr/testify v1.9.0
 	github.com/theplant/htmlgo v1.0.3
 	github.com/unrolled/secure v1.14.0
-	github.com/valyala/fasthttp v1.53.0
+	github.com/valyala/fasthttp v1.54.0
+	github.com/willoma/bulma-gomponents v0.11.1
+	github.com/willoma/gomplements v0.6.1
 	go.uber.org/zap v1.27.0
 	golang.org/x/net v0.25.0
 	google.golang.org/api v0.181.0
@@ -63,7 +67,7 @@ require (
 )
 
 require (
-	cloud.google.com/go v0.113.0 // indirect
+	cloud.google.com/go v0.114.0 // indirect
 	cloud.google.com/go/auth v0.4.2 // indirect
 	cloud.google.com/go/auth/oauth2adapt v0.2.2 // indirect
 	cloud.google.com/go/compute/metadata v0.3.0 // indirect
@@ -87,7 +91,7 @@ require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/glebarez/go-sqlite v1.22.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
@@ -178,7 +182,7 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
-	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/exp v0.0.0-20240525044651-4c93da0ed11d // indirect
 	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/oauth2 v0.20.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
@@ -197,7 +201,7 @@ require (
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.6 // indirect
 	gorm.io/hints v1.1.2 // indirect
-	modernc.org/libc v1.50.8 // indirect
+	modernc.org/libc v1.50.9 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
 	modernc.org/memory v1.8.0 // indirect
 	modernc.org/sqlite v1.29.10 // indirect
---
+++ gomponents/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"time"
+
+	g "github.com/maragudk/gomponents"
+	hx "github.com/maragudk/gomponents-htmx"
+	hxhttp "github.com/maragudk/gomponents-htmx/http"
+	c "github.com/maragudk/gomponents/components"
+	h "github.com/maragudk/gomponents/html"
+	ghttp "github.com/maragudk/gomponents/http"
+	b "github.com/willoma/bulma-gomponents"
+	e "github.com/willoma/gomplements"
+)
+
+func main() {
+	if err := start(); err != nil {
+		log.Fatalln("Error:", err)
+	}
+}
+
+func start() error {
+	now := time.Now()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", ghttp.Adapt(
+		func(w http.ResponseWriter, r *http.Request) (g.Node, error) {
+			if r.Method == http.MethodPost && hxhttp.IsBoosted(r.Header) {
+				now = time.Now()
+
+				hxhttp.SetPushURL(w.Header(), "/?time="+now.Format(timeFormat))
+
+				return partial(now), nil
+			}
+			return page(now), nil
+		},
+	))
+
+	mux.HandleFunc("/2", ghttp.Adapt(
+		func(w http.ResponseWriter, r *http.Request) (g.Node, error) {
+			return page2(), nil
+		},
+	))
+
+	log.Println("Starting on http://localhost:8000")
+	if err := http.ListenAndServe("localhost:8000", mux); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	return nil
+}
+
+const timeFormat = "15:04:05"
+
+func page(now time.Time) g.Node {
+	return c.HTML5(
+		c.HTML5Props{
+			Title: now.Format(timeFormat),
+			Head: []g.Node{
+				h.Script(h.Src("https://unpkg.com/htmx.org")),
+			},
+			Body: []g.Node{
+				b.Container(
+					b.Notification(
+						b.Primary,
+						"This container is ", e.Strong("centered"), " on desktop and larger viewports.",
+					),
+				),
+				h.Div(
+					h.Class("max-w-7xl mx-auto p-4 prose lg:prose-lg xl:prose-xl"),
+					h.H1(g.Text(`gomponents + HTMX`)),
+					h.P(g.Textf(`Time at last full page refresh was %v.`, now.Format(timeFormat))),
+					partial(now),
+					h.FormEl(
+						h.Method("post"),
+						h.Action("/"),
+						hx.Boost("true"), hx.Target("#partial"), hx.Swap("outerHTML"),
+						h.Button(h.Type("submit"), g.Text(`Update time`),
+							h.Class("rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"),
+						),
+					),
+				),
+			},
+		},
+	)
+}
+
+func partial(now time.Time) g.Node {
+	return h.P(
+		h.ID("partial"),
+		g.Textf(`Time was last updated at %v.`, now.Format(timeFormat)),
+	)
+}
+
+func Navbar() g.Node {
+	return b.Navbar(
+		b.Dark,
+		b.NavbarStart(
+			b.NavbarAHref("/", "Home"),
+			b.NavbarAHref("/about", "About"),
+		),
+	)
+}
+
+func page2() g.Node {
+	return b.HTML(
+		b.Script("https://unpkg.com/htmx.org"),
+		b.CSSPath("https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css"),
+		b.HTitle("Olala"),
+		b.Language("en"),
+		b.Head(e.Meta(e.Charset("utf-8"))),
+		b.Container(
+			b.Navbar(
+				b.NavbarBrand(),
+				b.NavbarAHref("/", "Home"),
+				b.NavbarAHref("/2", "22"),
+			),
+			b.Content(
+				e.H1("Hello"),
+				e.P("Hello world"),
+			),
+		),
+	)
+}
