503e238deb94cf69326f0aef3ed6abd7a448b8de
bibenga <{ID}+{username}@users.noreply.github.com>
2024-10-28 18:15:29 +0000

    fix

---
+++ queue/queue.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type Item struct {
+	priority int    // The priority of the item in the queue.
+	value    string // The value of the item; arbitrary.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	// return pq[i].priority > pq[j].priority
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	item := x.(*Item)
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil // don't stop the GC from reclaiming the item eventually
+	*pq = old[0 : n-1]
+	return item
+}
+
+func main() {
+	items := map[string]int{"banana": 3, "apple": 2, "pear": 4}
+
+	// Create a priority queue, put the items in it, and
+	// establish the priority queue (heap) invariants.
+	pq := make(PriorityQueue, len(items))
+	i := 0
+	for value, priority := range items {
+		pq[i] = &Item{value: value, priority: priority}
+		i++
+	}
+	heap.Init(&pq)
+
+	// Insert a new item and then modify its priority.
+	item := &Item{value: "orange", priority: 1}
+	heap.Push(&pq, item)
+
+	// Take the items out; they arrive in decreasing priority order.
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*Item)
+		fmt.Printf("%.2d:%s ", item.priority, item.value)
+	}
+
+}
