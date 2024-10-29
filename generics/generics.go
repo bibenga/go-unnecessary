d3f5131c20605b7b3c078a6b472ac9dab846118c
bibenga <{ID}+{username}@users.noreply.github.com>
2024-10-29 10:41:10 +0000

    fix

--- generics/generics.go
+++ generics/generics.go
@@ -15,8 +15,8 @@ func SumIntsOrFloats[K comparable, V int64 | float32 | float64](m map[K]V) V {
 }
 
 type Key struct {
-	p1 int
-	p2 int
+	p1 uint32
+	p2 uint32
 }
 
 func main() {
@@ -38,22 +38,18 @@ func main() {
 	log.Printf("floats -> %v", SumIntsOrFloats(floats))
 
 	//
-	vector := map[[2]int16]int64{
-		{1, 1}: 1,
-		{2, 2}: 2,
-	}
-	vector[[2]int16{3, 3}] = 3
+	vector := map[[2]uint16]int64{{1, 1}: 1, {2, 2}: 2}
+	vector[[2]uint16{3, 3}] = 3
 	log.Printf("vector -> %v", vector)
-	vector[[2]int16{3, 3}] = 4
-	log.Printf("vector -> %v", vector)
+	vector[[2]uint16{3, 3}] = 4
+	log.Printf("vector -> %#v", vector)
 
 	//
-	vector2 := map[Key]int64{
-		{1, 1}: 1,
-		{2, 2}: 2,
-	}
+	vector2 := map[Key]int64{{1, 1}: 1, {2, 2}: 2}
 	vector2[Key{3, 3}] = 3
 	log.Printf("vector2 -> %v", vector2)
 	vector2[Key{3, 3}] = 4
-	log.Printf("vector2 -> %v", vector2)
+	log.Printf("vector2 -> %#v", vector2)
+	delete(vector2, Key{1, 1})
+	log.Printf("vector2 -> %#v", vector2)
 }
