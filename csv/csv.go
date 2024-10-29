cb932c8a4f718a5930096eda1cc62f91360ace41
bibenga <{ID}+{username}@users.noreply.github.com>
2024-10-29 11:31:57 +0000

    play

--- csv/csv.go
+++ csv/csv.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"io"
@@ -22,19 +23,36 @@ func main() {
 	}
 	defer f.Close()
 
-	csvReader := csv.NewReader(f)
+	reader := bufio.NewReader(f)
+	bom := [3]byte{0xEF, 0xBB, 0xBF}
+	utf8sigHeader := make([]byte, 3)
+	n, err := f.Read(utf8sigHeader)
+	if err != nil {
+		panic(err)
+	}
+	if n != 3 {
+		panic("n != 3")
+	}
+	if utf8sigHeader[0] == bom[0] && utf8sigHeader[1] == bom[1] && utf8sigHeader[2] == bom[2] {
+		fmt.Println("hay utf8sigHeader")
+	} else {
+		fmt.Println("no hay utf8sigHeader")
+		f.Seek(0, 0)
+	}
+
+	csvReader := csv.NewReader(reader)
 	csvReader.Comma = ';'
 
 	headers, err := csvReader.Read()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", headers)
+	fmt.Printf("%#v\n", headers)
 	headersMap := map[string]int{}
 	for index, name := range headers {
 		headersMap[name] = index
 	}
-	fmt.Printf("%+v\n", headersMap)
+	fmt.Printf("%#v\n", headersMap)
 
 	var items []*Item
 	for {
@@ -67,32 +85,45 @@ func main() {
 		items = append(items, item)
 	}
 	fmt.Printf("%#v\n", items)
+	for _, item := range items {
+		fmt.Printf("%#v\n", item)
+	}
 
-	f2, err := os.Create("output.csv")
+	f2, err := os.Create("csv/output.csv")
 	if err != nil {
 		panic(err)
 	}
 	defer f2.Close()
 
-	writer := csv.NewWriter(f2)
-	defer writer.Flush()
+	// writer := bufio.NewWriter(f2)
+	if _, err := f2.Write([]byte{0xEF, 0xBB, 0xBF}); err != nil {
+		panic(err)
+	}
 
-	if err := writer.Write(headers); err != nil {
+	csvWriter := csv.NewWriter(f2)
+	csvWriter.Comma = ';'
+	csvWriter.UseCRLF = true
+	defer csvWriter.Flush()
+
+	if err := csvWriter.Write(headers); err != nil {
 		panic(err)
 	}
 
 	for _, item := range items {
 		var row []string = make([]string, len(headers))
 		for i, name := range headers {
-			if name == "Id" {
+			switch name {
+			case "Id":
 				row[i] = strconv.Itoa(item.Id)
-			} else if name == "Name" {
+			case "Name":
 				row[i] = item.Name
-			} else if name == "Age" {
+			case "Age":
 				row[i] = strconv.FormatFloat(item.Age, 'f', -1, 64)
+			default:
+				panic(fmt.Sprintf("Unknown name: %s", name))
 			}
 		}
-		if err := writer.Write(row); err != nil {
+		if err := csvWriter.Write(row); err != nil {
 			panic(err)
 		}
 	}
