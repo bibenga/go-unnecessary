451764985c276cf2aaab65cf00cc7e8d5e29a6b0
bibenga <{ID}+{username}@users.noreply.github.com>
2024-10-29 11:11:40 +0000

    play

--- csv/csv.go
+++ csv/csv.go
@@ -5,7 +5,15 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type Item struct {
+	Id   int
+	Name string
+	Age  float64
+}
 
 func main() {
 	f, err := os.Open("csv/csv.csv")
@@ -16,6 +24,19 @@ func main() {
 
 	csvReader := csv.NewReader(f)
 	csvReader.Comma = ';'
+
+	headers, err := csvReader.Read()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", headers)
+	headersMap := map[string]int{}
+	for index, name := range headers {
+		headersMap[name] = index
+	}
+	fmt.Printf("%+v\n", headersMap)
+
+	var items []*Item
 	for {
 		rec, err := csvReader.Read()
 		if err == io.EOF {
@@ -24,7 +45,56 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		// do something with read line
-		fmt.Printf("%+v\n", rec)
+
+		id, err := strconv.Atoi(rec[headersMap["Id"]])
+		if err != nil {
+			panic(err)
+		}
+
+		name := rec[headersMap["Name"]]
+
+		ageS := rec[headersMap["Age"]]
+		age, err := strconv.ParseFloat(ageS, 64)
+		if err != nil {
+			age, err = strconv.ParseFloat(strings.Replace(ageS, ",", ".", 1), 64)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		item := &Item{Id: id, Name: name, Age: age}
+		fmt.Printf("%#v\n", item)
+		items = append(items, item)
 	}
+	fmt.Printf("%#v\n", items)
+
+	f2, err := os.Create("output.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f2.Close()
+
+	writer := csv.NewWriter(f2)
+	defer writer.Flush()
+
+	if err := writer.Write(headers); err != nil {
+		panic(err)
+	}
+
+	for _, item := range items {
+		var row []string = make([]string, len(headers))
+		for i, name := range headers {
+			if name == "Id" {
+				row[i] = strconv.Itoa(item.Id)
+			} else if name == "Name" {
+				row[i] = item.Name
+			} else if name == "Age" {
+				row[i] = strconv.FormatFloat(item.Age, 'f', -1, 64)
+			}
+		}
+		if err := writer.Write(row); err != nil {
+			panic(err)
+		}
+	}
+
 }
